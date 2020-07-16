808d26a959913b1631c1cc2b5708762d6b4e230c
nilah-m2021 <67973031+nilah-m2021@users.noreply.github.com>
2020-07-16 09:52:18 -0500

    1st Commit
    1st Commit

---
+++ Nilah_M_Simple Looping.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var numberOfAnts = 1
+
+while(numberOfAnts <= 10){
+    print("The ants go marching \(numberOfAnts) by \(numberOfAnts), Hoorah! Hoorah! \n" +
+    "The ants go marching \(numberOfAnts) by \(numberOfAnts), Hoorah! Hoorah! \n" +
+    "The ants go marching \(numberOfAnts) by \(numberOfAnts) \n" +
+    "The little one stops to suck his thumb. \n" +
+    "And they all go marching down to the ground. \n" +
+    "To get out of the rain. \n" +
+    "Boom, boom, boom, boom! \n")
+    numberOfAnts += 1
+}
